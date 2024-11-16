08313d2e5a9ed1370debf6c78a0e891bda353666
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-15 21:38:02 -0500

    cached network image

--- lib/pages/tabs_pages/chats_tab_page.dart
+++ lib/pages/tabs_pages/chats_tab_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsappg10/pages/widgets/chat_widget.dart';
 
@@ -6,6 +7,27 @@ class ChatsTabPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: [
+        Container(
+          child: CachedNetworkImage(
+            progressIndicatorBuilder: (context, url, progress) => Center(
+              child: CircularProgressIndicator(
+                value: progress.progress,
+              ),
+            ),
+            imageUrl:
+                "https://images.pexels.com/photos/27198641/pexels-photo-27198641/free-photo-of-paisaje-naturaleza-vacaciones-arena.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            imageBuilder: (context, imageProvider) => Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ),
         ChatWidget(),
         ChatWidget(),
         ChatWidget(),
--- lib/pages/widgets/chat_widget.dart
+++ lib/pages/widgets/chat_widget.dart
@@ -1,15 +1,38 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class ChatWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: CircleAvatar(
-        backgroundImage: NetworkImage(
-          "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg",
+      leading: SizedBox(
+        height: 56,
+        width: 56,
+        child: CachedNetworkImage(
+          imageUrl:
+              "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg",
+          progressIndicatorBuilder: (context, url, progress) => Center(
+            child: Container(
+              child: CircularProgressIndicator(
+                color: Colors.green,
+                value: progress.progress,
+                strokeWidth: 5,
+              ),
+            ),
+          ),
+          imageBuilder: (context, imageProvider) => CircleAvatar(
+            backgroundImage: imageProvider,
+            radius: 28,
+          ),
         ),
-        radius: 32,
       ),
+
+      //  CircleAvatar(
+      //   backgroundImage: NetworkImage(
+      //     "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg",
+      //   ),
+      //   radius: 32,
+      // ),
       title: Text(
         "Nombre de la person",
         style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
