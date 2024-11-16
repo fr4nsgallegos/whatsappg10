0239da135fa07bd66f0a7fd42039fa1c6690933b
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-15 21:18:16 -0500

    call page

--- lib/pages/tabs_pages/calls_tab_page.dart
+++ lib/pages/tabs_pages/calls_tab_page.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:whatsappg10/pages/widgets/call_widget.dart';
 
 class CallsTabPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Calls tab page"),
+      child: ListView(
+        children: [
+          CallWidget(),
+          CallWidget(),
+          CallWidget(),
+          CallWidget(),
+          CallWidget(),
+          CallWidget(),
+          CallWidget(),
+          CallWidget(),
+          CallWidget(),
+        ],
+      ),
     );
   }
 }
---
+++ lib/pages/widgets/call_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CallWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(
+            "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg",
+          ),
+          radius: 32,
+        ),
+        title: Text(
+          "Juan Manuel",
+          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Row(
+          children: [
+            Icon(
+              Icons.call_made,
+              color: Colors.greenAccent,
+            ),
+            Text("Yesterdar 15:15"),
+          ],
+        ),
+        trailing: Icon(
+          Icons.call,
+          color: Colors.green,
+        ));
+  }
+}
