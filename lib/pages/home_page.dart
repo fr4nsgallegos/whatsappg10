e17215ecfb5d6271a4f08528d22502b98d220dac
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-15 19:37:47 -0500

    tabbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,17 +3,33 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // backgroundColor: Color(0xff1D3D2C),
-        title: Text("WhatsApp"),
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
-        ],
-        bottom: TabBar(tabs: []),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("WhatsApp"),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+          ],
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.camera_alt)),
+              Tab(text: "Chats"),
+              Tab(text: "Status"),
+              Tab(text: "Calls"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Center(child: Text("Camera")),
+            Center(child: Text("Chats Page")),
+            Center(child: Text("Status Page")),
+            Center(child: Text("Calls Page")),
+          ],
+        ),
       ),
-      body: Column(),
     );
   }
 }
