f3e89c45930abd48915c19911f28fb9d3b9f5d8f
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-15 19:32:33 -0500

    appbar

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,13 @@ void main() {
   runApp(
     MaterialApp(
       home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Color(0xff1D3D2C),
+          foregroundColor: Colors.white,
+        ),
+      ),
     ),
   );
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,7 +4,15 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        // backgroundColor: Color(0xff1D3D2C),
+        title: Text("WhatsApp"),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+        ],
+        bottom: TabBar(tabs: []),
+      ),
       body: Column(),
     );
   }
